df2794377bf8725ddddd7b2d2fdd833c3cf5ff63
Farhan Laeeq <farhan.laeeq@synapse.com>
2019-08-20 11:03:13 +0530

    updated spring security dependency

---
+++ swagger-tutorial/src/main/java/com/demo/swagger/config/SpringSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.demo.swagger.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
